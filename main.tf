43fda08e3bc2b630aa8c112d4b42d617dadfae66
Gillian Stravers <gillianstravers@icloud.com>
2024-10-15 15:19:35 +0200

    version 0.0.2

--- main.tf
+++ main.tf
@@ -1,39 +1,28 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group.name
-  location = var.resource_group.location
-  tags = merge(
-    try(var.tags),
-    tomap({
-      "Resource Type" = "Resource Group"
-    })
-  )
+    name     = var.resource_group.name
+    location = var.resource_group.location
 }
+
 resource "azurerm_recovery_services_vault" "vault" {
-    name                = var.vault.name
-    location            = var.vault.location
-    resource_group_name = azurerm_resource_group.rg.name
-    sku                 = var.vault.sku
-    tags = merge(
-        try(var.tags),
-        tomap({
-            "Resource Type" = "Recovery Services Vault"
-        })
-    )
+    name                          = var.vault.name
+    location                      = var.vault.location
+    resource_group_name           = azurerm_resource_group.rg.name
+    sku                           = var.vault.sku
+    soft_delete_enabled           = var.vault.soft_delete_enabled
+    public_network_access_enabled = var.vault.public_network_access_enabled
+    purge_protection_enabled      = var.vault.purge_protection_enabled
 }
 
 resource "azurerm_storage_account" "sa" {
-    name                     = var.storage_account.name
-    resource_group_name      = azurerm_resource_group.rg.name
-    location                 = var.storage_account.location
-    account_tier             = var.storage_account.account_tier
-    account_replication_type = var.storage_account.account_replication_type
-    account_kind             = "storagev2"
-    tags = merge(
-        try(var.tags),
-        tomap({
-            "Resource Type" = "Storage Account"
-        })
-    )
+    name                          = var.storage_account.name
+    resource_group_name           = azurerm_resource_group.rg.name
+    location                      = var.storage_account.location
+    access_tier =  var.storage_account.access_tier
+    account_tier                  = var.storage_account.account_tier
+    account_replication_type      = var.storage_account.account_replication_type
+    account_kind                  = "storagev2"
+    min_tls_version               = var.storage_account.min_tls_version
+    public_network_access_enabled = var.storage_account.public_network_access_enabled
 }
 
 resource "azurerm_private_endpoint" "recovery_services_vault_pe" {
--- variables.tf
+++ variables.tf
@@ -1,18 +1,60 @@
 variable "resource_group" {
   description = "Resource Group"
-  type        = object({
+  type = object({
     name     = string
     location = string
   })
 }
 
+variable "azurerm_recovery_services_vault" {
+  description = "Recovery Services Vault"
+  type = object({
+    name                          = string
+    location                      = string
+    sku                           = string
+    soft_delete_enabled           = bool
+    public_network_access_enabled = bool
+  })
+  default = {
+    sku                           = "standard"
+    soft_delete_enabled           = true
+    public_network_access_enabled = false
+  }
+}
+
+
 variable "storage_account" {
-    description = "Storage Account"
-    type        = object({
-        name                     = string
-        location                 = string
-        account_tier             = string
-        account_replication_type = string
-    })
-  
+  description = "Storage Account"
+  type = object({
+    name                          = string
+    location                      = string
+    access_tier                   = string
+    account_tier                  = string
+    account_replication_type      = string
+    min_tls_version               = string
+    public_network_access_enabled = bool
+  })
+  default = {
+    min_tls_version               = "1.2"
+    public_network_access_enabled = false
+  }
+}
+
+variable "recovery_services_vault_pe" {
+  description = "Recovery Services Vault Private Endpoint"
+  type = object({
+    name      = string
+    location  = string
+    subnet_id = string
+  })
+
+}
+
+variable "storage_account_pe" {
+  description = "Storage Account Private Endpoint"
+  type = object({
+    name      = string
+    location  = string
+    subnet_id = string
+  })
 }
