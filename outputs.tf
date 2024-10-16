7e5ba92c04bb5b2e1fc97aad843d96ab8ad48d3e
Gillian Stravers <gillianstravers@icloud.com>
2024-10-16 09:26:32 +0200

    added resourceID outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "azurerm_resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "azurerm_recovery_services_vault_id" {
+  value = azurerm_recovery_services_vault.vault.id
+}
+
+output "azurerm_storage_account_id" {
+  value = azurerm_storage_account.sa.id
+}
+
+output "azurerm_private_endpoint_recovery_services_vault_id" {
+  value = azurerm_private_endpoint.recovery_services_vault_pe.id
+}
+
+output "azurerm_private_endpoint_storage_account_id" {
+  value = azurerm_private_endpoint.storage_account_pe.id
+}
